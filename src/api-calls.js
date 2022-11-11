b85ccecb87668214d711fe6538063293eec6375d
martin13s18 <117728421+MaRtin322s@users.noreply.github.com>
2022-11-11 17:59:30 +0200

    login user

--- src/api-calls.js
+++ src/api-calls.js
@@ -8,3 +8,14 @@ export const registerUser = (userdata) => {
     })
     .then(res => res.json());
 }
+
+export const loginUser = (userData) => {
+    return fetch('http://localhost:3030/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    })
+    .then(res => res.json());
+}
--- src/views/loginView.js
+++ src/views/loginView.js
@@ -1,9 +1,12 @@
+import page from "../../node_modules/page/page.mjs";
 import { html } from "../../node_modules/lit-html/lit-html.js";
+import { loginUser } from "../api-calls.js";
+import { changeNav } from "../middlewares/middleware.js";
 
 const loginView = () => html`
     <section id="login">
         <div class="container">
-            <form id="login-form" action="#" method="post">
+            <form id="login-form" @submit=${loginHandler}>
                 <h1>Login</h1>
                 <p>Please enter your credentials.</p>
                 <hr>
@@ -26,4 +29,22 @@ const loginView = () => html`
 
 export const renderLogin = (ctx) => {
     ctx.rendering(loginView());
+}
+
+function loginHandler(ev) {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.currentTarget);
+    const username = formData.get('username');
+    const password = formData.get('password');
+    const data = { username, password };
+
+    loginUser(data)
+        .then(user => {
+            localStorage.setItem('_id', user._id);
+            localStorage.setItem('username', user.username);
+            localStorage.setItem('accessToken', user.accessToken);
+            page.redirect('/');
+            changeNav();
+        })
 }
